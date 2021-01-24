1ff7ffd9e8e0ec90eba26b9359eb61895468c859
Florian <mesagouiffes@gmail.com>
2021-01-24 16:12:36 +0100

    create base component Bar and Block

---
+++ src/components/bar/Bar.component.tsx
@@ -0,0 +1,5 @@
+import style from "./Bar.module.scss";
+
+export default function Bar() {
+  return <div>Bar component</div>;
+}
---
+++ src/components/block/Block.component.tsx
@@ -0,0 +1,5 @@
+import style from "./Block.module.scss";
+
+export default function Block() {
+  return <div>Block component</div>;
+}
