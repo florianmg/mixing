fd04a0de0ab110bbf01a3b519f89d013341e0d4f
Florian <mesagouiffes@gmail.com>
2021-02-21 18:01:28 +0100

    feat/ filter by one color

--- pages/classic.tsx
+++ pages/classic.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ClassicBlock from "../src/components/classic-block";
 import GroupColor from "../src/components/group-color";
@@ -22,23 +22,42 @@ export async function getStaticProps() {
 }
 
 const Classic: React.FC<ClassicPageProps> = ({ originalClay, mixedClay }) => {
+  const [originalClaySelected, setOriginalClaySelected] = useState([]);
+
+  const checkIfSelected = (items): boolean => {
+    if (originalClaySelected.length === 0) return true;
+    return (
+      originalClaySelected.includes(items[0].bases[0].fimoId) ||
+      originalClaySelected.includes(items[0].bases[1].fimoId)
+    );
+  };
+
   return (
     <div className={style.pageContainer} key="classic-page">
       <h3>Original Clay's</h3>
       <div className={style.originals}>
         {originalClay.map(({ color, fimoId }) => (
-          <ClassicBlock key={fimoId} blockKey={fimoId} color={color} />
+          <ClassicBlock
+            key={fimoId}
+            blockKey={fimoId}
+            color={color}
+            selected={originalClaySelected.includes(fimoId)}
+            handleSelectFilter={() => setOriginalClaySelected([fimoId])}
+          />
         ))}
       </div>
       <div className={style["mix-container"]}>
         <h3>Mixed Clay</h3>
         <div className={style["mix-list"]}>
-          {mixedClay.map((items) => (
-            <GroupColor
-              mixedList={items}
-              key={`group-color${items[0].mixedId}`}
-            />
-          ))}
+          {mixedClay.map(
+            (items) =>
+              checkIfSelected(items) && (
+                <GroupColor
+                  mixedList={items}
+                  key={`group-color${items[0].mixedId}`}
+                />
+              )
+          )}
         </div>
       </div>
     </div>
--- src/components/classic-block/ClassicBlock.component.tsx
+++ src/components/classic-block/ClassicBlock.component.tsx
@@ -1,11 +1,21 @@
 import style from "./ClassicBlock.module.scss";
 
-export default function ClassicBlock({ blockKey, color }) {
+export default function ClassicBlock({
+  blockKey,
+  color,
+  selected,
+  handleSelectFilter,
+}) {
   return (
     <div
       key={blockKey}
+      onClick={handleSelectFilter}
       className={style["classic-block"]}
-      style={{ backgroundColor: color }}
+      style={
+        selected
+          ? { backgroundColor: color, borderColor: "red", borderStyle: "solid" }
+          : { backgroundColor: color }
+      }
     >
       <p className={style.text}>{blockKey}</p>
     </div>
