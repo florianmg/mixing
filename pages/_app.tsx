5ec9ffc3c17c66a97a9fdf9c2f442bba93579f29
Florian <mesagouiffes@gmail.com>
2021-02-26 19:30:43 +0100

    Home page design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,5 @@ import Navbar from "../src/components/nav-bar";
 import "../styles/globals.scss";
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
